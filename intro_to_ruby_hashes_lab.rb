8e770b41b1b01cffc9be81bb90c629777e914554
Chen Wang <wangcees@gmail.com>
2019-08-18 16:31:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   new_hash = monopoly_with_second_tier
   if new_hash[:railroads][:rent_in_dollars]
   else
-    new_hash[:railroads][:names][:rent_in_dollars] = {
+    new_hash[:railroads][:rent_in_dollars] = {
       :owner => 3,
       :owner2 => 3,
       :owner3 => 4,
